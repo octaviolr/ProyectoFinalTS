127740a10ee307e1e6daabd34f950329269717bf
octaviolr <octavio987@hotmail.com>
2022-05-14 20:59:56 -0500

    Add files via upload

---
+++ PF0.4/Códigos/LogicaCambioEscena.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LogicaCambioEscena : MonoBehaviour
+{
+    public bool cambio;
+    public int indice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CambiarEscena(indice);
+        }
+
+        if(cambio)
+        {
+            CambiarEscena(indice);
+        }
+    }
+
+    public void CambiarEscena(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
---
+++ PF0.4/Códigos/LogicaConejo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaConejo : MonoBehaviour
+{
+
+    public float VelocidadMov = 5.0f;
+    public float VelocidadGiro = 200.0f;
+    private Animator anim;
+    public float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.GetComponent<Animator>();
+
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * VelocidadGiro, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * VelocidadMov);
+
+    }
+}
